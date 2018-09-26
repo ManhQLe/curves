d3940ff0843a8f2f3e7e2b7bd3428ba14b935cf6
Manh Le <quang.manh@gmail.com>
2018-09-25 22:21:57 -0500

    reworked Vec render

--- src/content/js/geo.js
+++ src/content/js/geo.js
@@ -25,33 +25,57 @@ class ThreePointCurve {
 
 class Vec{
     constructor(p1,p2){        
-        this.P1 = p1;
-        this.P2 = p2;
+        this.P = [p1,p2];
+        Object.defineProperties(this,{
+            "P1":{
+                get:function(){return this.P[0]},
+                set:function(v){this.P[0] = v}
+            },
+            "P2":{
+                get:function(){return this.P[1]},
+                set:function(v){this.P[1] = v}
+            }
+        })        
+    }
+    refresh(){
+        let g = this.vessel;
+        g.select("line")
+        .attr("x1",d=>d.P1[0])
+        .attr("y1",d=>d.P1[1])
+        .attr("x2",d=>d.P2[0])
+        .attr("y2",d=>d.P2[1])
+
+        g.selectAll("circle")
+        .data(vec.P)
+        .enter()
+        .append("circle").attr("fill","#3498db")
+        .attr("cx",d=>d[0])
+        .attr("cy",d=>d[1])
+        .attr("r","5")
+    }
+    present(canvas){
+        this.vessel = Vec.render(canvas, this);
     }
 
     static render(canvas,vec){
         let g = cavas.append("g");
         let line = g.append("line")
+        .datum(vec)
         .attr("stroke","#f39c12")
         .attr("stroke-width","2")
-        .attr("x1",vec.P1[0])
-        .attr("y1",vec.P1[1])
-        .attr("x2",vec.P2[0])
-        .attr("y2",vec.P2[1])
+        .attr("x1",d=>d.P1[0])
+        .attr("y1",d=>d.P1[1])
+        .attr("x2",d=>d.P2[0])
+        .attr("y2",d=>d.P2[1])
 
-        g.append("circle").attr("fill","#3498db")
-        .attr("cx",vec.P1[0])
-        .attr("cy",vec.P1[1])
-        .attr("r","10")
-
-        g.append("circle").attr("fill","#3498db")
-        .attr("cx",vec.P2[0])
-        .attr("cy",vec.P2[1])
-        .attr("r","10")
-
-
-        
-        
+        g.selectAll("circle")
+        .data(vec.P)
+        .enter()
+        .append("circle").attr("fill","#3498db")
+        .attr("cx",d=>d[0])
+        .attr("cy",d=>d[1])
+        .attr("r","5")
+        return g;        
     }
 }
 
